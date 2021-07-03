e98f1bc032f27329dd0acb5081a80a6171697ace
Kai <kaikunze@me.com>
2021-07-03 21:26:26 +0900

    config command in fish function and new color sheme from Omy fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,6 @@
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
